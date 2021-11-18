d9abd98d89c79b374735aff80fd1baa55e253a76
kohh <yosuke@hatno.com>
2021-11-19 01:31:39 +0900

    プロジェクト ファイルを追加します。

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace securityManager_fp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(55, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.richTextBox1.Location = new System.Drawing.Point(363, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(329, 417);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(201, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "起動";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(201, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "起動";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "●";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "●";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 105);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "各棟の\r\nステータス表示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 335);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 105);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "地図で表示";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(53, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "複数建物全体";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(374, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "▼ 出力ログ ▼";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(126, 224);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(108, 105);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "出力ログ\r\nリセット(アプリ内)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(126, 335);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 105);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "正常化\r\n(アラーム停止)";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(240, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(108, 105);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 134);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "▼ 建物を選択 ▼";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 176);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 19);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "memo";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(240, 336);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(108, 105);
+            this.button8.TabIndex = 20;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(710, 451);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.MinimumSize = new System.Drawing.Size(725, 490);
+            this.Name = "Form1";
+            this.Text = "security manager fp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace securityManager_fp
+{
+    public partial class Form1 : Form
+    {
+        List<string> BLDs = new List<string>() { "1号館","新2号館","8号館","10号館"};
+        //要素は例．あとでpythonのプログラムのやり方に合わせて取得
+        Dictionary<string, int> BLDstate = new Dictionary<string, int>();
+        //0でセンサー反応,1で異常なし,2がスリープ状態
+
+        Boolean isSleepingAll = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0;i < BLDs.Count; i++)
+            {
+                BLDstate.Add(BLDs[i], 2);
+            }
+            //あとで変更されるだろう部分2
+
+            comboBox1.Items.AddRange(BLDs.ToArray());
+            richTextBox1.AppendText(timeStamp()+"各建物のセンサー状態を管理します\n");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null) {
+                if (BLDstate[comboBox1.SelectedItem.ToString()] == 2)
+                {
+                    richTextBox1.AppendText(timeStamp());
+                    richTextBox1.SelectionColor = Color.FromArgb(0,153,0);
+                    richTextBox1.AppendText(label2.Text);
+                    richTextBox1.SelectionColor = Color.Black;
+                    richTextBox1.AppendText(" " + comboBox1.SelectedItem.ToString() + "の監視を開始します\n");
+                    BLDstate[comboBox1.SelectedItem.ToString()] = 1;
+                    label2.ForeColor = Color.FromArgb(0, 153, 0);
+                    button1.Text = "停止";
+                    //ここでimを使ってスリープを送り続けるのを止める
+                }
+                else
+                {
+                    richTextBox1.AppendText(timeStamp());
+                    richTextBox1.SelectionColor = Color.FromArgb(32, 32, 32);
+                    richTextBox1.AppendText(label2.Text);
+                    richTextBox1.SelectionColor = Color.Black;
+                    richTextBox1.AppendText(" " + comboBox1.SelectedItem.ToString() + "を待機状態にします\n");
+                    BLDstate[comboBox1.SelectedItem.ToString()] = 2;
+                    label2.ForeColor = Color.FromArgb(32, 32, 32);
+                    button1.Text = "起動";
+                    //ここでimを使ってスリープを送らせ続ける
+                }
+            }
+            else
+            {
+                richTextBox1.AppendText(timeStamp()+"建物が選択されていません\n");
+            }
+        }
+
+        static string timeStamp()
+        {
+            DateTime dt = DateTime.Now;
+            string stamp = dt.ToString("[yyyy/MM/dd HH:mm:ss] ");
+
+            return stamp;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (BLDstate[comboBox1.SelectedItem.ToString()])
+            {
+                case 0:
+                    label2.ForeColor = Color.FromArgb(232,0,43);
+                    button1.Text = "停止";
+                    break;
+                case 1:
+                    label2.ForeColor = Color.FromArgb(0, 153, 0);
+                    button1.Text = "停止";
+                    break;
+                default:
+                    label2.ForeColor = Color.FromArgb(32, 32, 32);
+                    button1.Text = "起動";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (isSleepingAll == true)
+            {
+                richTextBox1.AppendText(timeStamp());
+                richTextBox1.SelectionColor = Color.FromArgb(0, 153, 0);
+                richTextBox1.AppendText(label2.Text);
+                richTextBox1.SelectionColor = Color.Black;
+                richTextBox1.AppendText(" 全棟の監視を開始します\n");
+
+                for (int i = 0; i < BLDs.Count; i++)
+                {
+                    BLDstate[BLDs[i]] = 1;
+                }
+
+                label2.ForeColor = Color.FromArgb(0, 153, 0);
+                label3.ForeColor = Color.FromArgb(0, 153, 0);
+                button1.Text = "停止";
+                isSleepingAll = false;
+                //ここでimを使ってスリープを止める
+            }
+            else
+            {
+
+                richTextBox1.AppendText(timeStamp() + "● 全棟を待機状態にします\n");
+
+                for (int i = 0; i < BLDs.Count; i++)
+                {
+                    BLDstate[BLDs[i]] = 2;
+                }
+
+                label2.ForeColor = Color.FromArgb(32, 32, 32);
+                label3.ForeColor = Color.FromArgb(32, 32, 32);
+                button1.Text = "起動";
+                isSleepingAll = true;
+                //ここでimを使って全体にスリープを送らせ続ける
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.AppendText(timeStamp()+ " 各棟のステータスを表示\n");
+
+            for(int i = 0; i < BLDs.Count; i++)
+            {
+                switch (BLDstate[BLDs[i]])
+                {
+                    case 0:
+                        richTextBox1.SelectionColor = Color.FromArgb(232, 0, 43);
+                        richTextBox1.AppendText("● ");
+                        richTextBox1.SelectionColor = Color.Black;
+                        break;
+                    case 1:
+                        richTextBox1.SelectionColor = Color.FromArgb(0, 153, 0);
+                        richTextBox1.AppendText("● ");
+                        richTextBox1.SelectionColor = Color.Black;
+                        break;
+                    default:
+                        richTextBox1.AppendText("● ");
+                        break;
+                }
+                richTextBox1.AppendText(BLDs[i]+"\n");
+            }
+
+            richTextBox1.AppendText("-------書き出し終了-------\n");
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                richTextBox1.AppendText(timeStamp() + "[memo]" + textBox1.Text + "\n");
+                textBox1.ResetText();
+            }
+        }
+    }
+}
